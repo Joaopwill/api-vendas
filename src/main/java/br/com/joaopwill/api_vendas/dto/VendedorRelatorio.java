f32a0fdfc3174c8c3c0488ad585973abf2f5ce66
João Pedro Silva Correa <joaocorrea.jar@gmail.com>
2025-10-08 12:27:27 -0300

    Criei a classe VendedorRelatorio

---
+++ src/main/java/br/com/joaopwill/api_vendas/dto/VendedorRelatorio.java
@@ -0,0 +1,15 @@
+package br.com.joaopwill.api_vendas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data //Anotação do Lombok para gerar getters e setters automáticos
+@AllArgsConstructor //Recebe todos os parametros em um unico construtor, evitando escrever codigo repetitivo
+
+public class VendedorRelatorio {
+    private String nomeVendedor;
+    private Long totalVendas;
+    private BigDecimal mediaVendas; //Média de vendas por DIA 
+}
