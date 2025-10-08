61a4ac32866c016d06e9e04725f653c19e177970
João Pedro Silva Correa <joaocorrea.jar@gmail.com>
2025-10-08 11:13:47 -0300

    Inicializei projeto Spring Boot com as dependencias jpa, H2, lombook, SpringWeb e Devtools.

---
+++ src/main/java/br/com/joaopwill/api_vendas/ApiVendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaopwill.api_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVendasApplication.class, args);
+	}
+
+}
